152db3f868d36111ac551212a3ddbd58e3e65435
MinZhi <chua.minzhi@gmail.com>
2022-06-24 10:16:39 +0200

    back to previous version

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 
 from applicationinsights.flask.ext import AppInsights
-from logging import StreamHandler
+# from logging import StreamHandler
 
 load_dotenv()
 
@@ -30,9 +30,9 @@ app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = '9fc0b1a2-8f7f-472e-b889-e346889e
 appinsights = AppInsights(app)
 
 
-# keep stdout/stderr logging using StreamHandler
-streamHandler = StreamHandler()
-app.logger.addHandler(streamHandler)
+# # keep stdout/stderr logging using StreamHandler
+# streamHandler = StreamHandler()
+# app.logger.addHandler(streamHandler)
 
 
 # force flushing application insights handler after each request
