d31de0f02cb369716a3190695c12b54318d551e4
MinZhi <chua.minzhi@gmail.com>
2022-07-01 13:04:49 +0200

    add class name for title in profile.html

--- test/mz_sel_test_login.py
+++ test/mz_sel_test_login.py
@@ -21,14 +21,22 @@ class Login_main_auth(unittest.TestCase):
         elem2 = driver.find_element(By.NAME, "password")
         elem2.send_keys("abcabc")
         # elem2.send_keys(Keys.RETURN)
-        time.sleep(5)
+        # time.sleep(5)
 
         elem3 = driver.find_element(By.ID, "btn_login")
         elem3.click()
 
-        time.sleep(5)
+        time.sleep(3)
 
         self.assertIn("http://mz-monitoring-flask-v2.azurewebsites.net/profile", driver.current_url)
+        # print(driver.page_source)
+
+        test_elem = driver.find_element(By.CLASS_NAME, "profile_title")
+        print (test_elem.text())
+
+        # self.assertEquals("Welcome ABC ABC", test_elem.getText())
+        # self.assertIn("Welcome ABC ABC" not in driver.page_source)
+
         # self.assertNotIn("No results found.", driver.page_source)
 
 
